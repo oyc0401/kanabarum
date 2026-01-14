50dc4eb3c63d4e8b9d1afb138f9102e378e9ff52
yuchan <oyc0401@gmail.com>
2026-01-14 19:34:50 +0900

    dist 제거

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-type KanaToHangul = (input: string) => string;
-declare function initKanaToHangul(): Promise<KanaToHangul>;
-declare function kanaToHangul(input: string): Promise<string>;
-declare const KanaBarum: Readonly<{
-    init: typeof initKanaToHangul;
-}>;
-
-export { type KanaToHangul, KanaBarum as KanaToHangulMaker, kanaToHangul };
